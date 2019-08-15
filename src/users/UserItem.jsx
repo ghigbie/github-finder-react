d3b0b808a87e3d9cf965e6978e602bc2da625bda
ghigbie <gehigbie@gmail.com>
2019-08-14 20:39:17 -0600

    Add two new componets

---
+++ src/users/UserItem.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class UserItem extends Component{
+    render(){
+        const { avatar_url, login, html_url } = this.props;
+        return(
+            <div className='card text-center'>
+                <img
+                    src={avatar_url}
+                    alt={avatar_url}
+                    className='round-img'
+                    style={{ width: '60px' }}/>
+                <h3>{login}</h3>
+                <div>
+                    <a 
+                        href={html_url}
+                        className='btn btn-dark btn-sm my'></a>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default UserItem;
---
+++ src/users/UsersList.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from './node_modules/react';
+import UserItem from './UserItem';
+
+class UserList extends Component{
+    render(){
+        return(
+            <div className="UserList">
+            
+            </div>
+        )
+    }
+}
