7a2d43a189de359f7aba133df586b751e0306732
ghigbie <gehigbie@gmail.com>
2019-08-15 19:40:27 -0600

    Edit functional component

--- src/Components/layout/Navbar.jsx
+++ src/Components/layout/Navbar.jsx
@@ -1,12 +1,12 @@
-import  React, {Component}  from 'react';
+import  React from 'react';
 import PropTypes from 'prop-types'
 
-const Navbar = () => {
+const Navbar = (props) => {
         return(
             <nav className='navbar bg-primary'>
                 <h1>
-                    <i className={this.props.icon}></i>
-                    {this.props.title}
+                    <i className={props.icon}></i>
+                    {props.title}
                 </h1>
             </nav>
         );
