e4469fd461138464e60abf721ee72fec8c99527c
ghigbie <gehigbie@gmail.com>
2019-08-17 18:52:58 -0600

    Add async await to search users function

--- src/App.js
+++ src/App.js
@@ -21,8 +21,13 @@ class App extends Component {
     });
   }
 
-  searchUsers = () => {
-
+  async searchUsers = () => {
+    this.setState({ loading: true});
+    const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({
+      users: res.data,
+      loading: false
+    })
   }
 
   render(){
