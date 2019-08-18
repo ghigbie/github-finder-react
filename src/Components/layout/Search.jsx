38edcf512fb456e171800ac8c215ab831ddcb83a
ghigbie <gehigbie@gmail.com>
2019-08-17 19:30:51 -0600

    Edit conditional rendering of users button

--- src/Components/layout/Search.jsx
+++ src/Components/layout/Search.jsx
@@ -9,6 +9,7 @@ export class Search extends Component {
     static propTypes = {
         searchUsers: PropTypes.func.isRequired,
         clearUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired,
     }
 
     onChange = e => this.setState({ [e.target.name]: e.target.value});
@@ -32,8 +33,10 @@ export class Search extends Component {
                            value="Search"
                            className="btn btn-dark btn-block"/>
                 </form>
-                <button className="btn btn-light btn-block"
-                        onClick={this.props.clearUsers}>Clear</button>
+                { this.props.showClear &&
+                    (<button className="btn btn-light btn-block"
+                             onClick={this.props.clearUsers}>Clear</button>)
+                }
             </div>
         )
     }
