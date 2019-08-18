d9a741430395bb9a6c4906402d32fe51c4337a21
ghigbie <gehigbie@gmail.com>
2019-08-18 14:21:23 -0600

    Add new destructured vars

--- src/Components/users/User.jsx
+++ src/Components/users/User.jsx
@@ -7,7 +7,24 @@ export class User extends Component {
         user: PropTypes.object.isRequired,
     }
 
+    componentDidMount(){
+        this.props.getUser({this.props.match.params.login});
+    }
+
     render() {
+        const {
+            name,
+            avatar_url,
+            location,
+            bio,
+            blog,
+            login,
+            html_url
+            followers,
+            following,
+            public_repos,
+            public_gist
+         } = this.props.user;
         return (
             <div class="User">
                 User
