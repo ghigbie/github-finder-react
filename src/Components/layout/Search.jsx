283022a124c3e9fa6f2bde963b259a010bcbfd89
ghigbie <gehigbie@gmail.com>
2019-08-17 18:39:36 -0600

    Add value for onchange

--- src/Components/layout/Search.jsx
+++ src/Components/layout/Search.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 
 export class Search extends Component {
+    state = {
+        text: ''
+    }
+
+    handleOnChange = (e) => {
+        this.setState(() => ({text: e.target.value}));
+    }
+
     render() {
         return (
             <div className='Search'>
                 <form className="form">
                     <input type="text"
                            name="text"
+                           value={this.state.text}
+                           onChange={this.handleOnChange}
                            placeholder="Search users..."/>
                     <input type="submit" 
                            value="Search"
