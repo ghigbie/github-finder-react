f904a5d2a174db948ada2993799ddb5670c38227
ghigbie <gehigbie@gmail.com>
2019-08-18 19:23:45 -0600

    Apply last two props

--- src/Components/users/User.jsx
+++ src/Components/users/User.jsx
@@ -18,7 +18,9 @@ class User extends Component {
     render() {
         const { //all from the user object
             name,
+            company,
             avatar_url,
+            hireable,
             location,
             bio,
             blog,
@@ -27,7 +29,7 @@ class User extends Component {
             followers,
             following,
             public_repos,
-            public_gist
+            public_gists
          } = this.props.user;
 
          const { loading } = this.props;
@@ -41,6 +43,62 @@ class User extends Component {
                     classNAme='btn btn-light'>
                     Back to Search
                 </Link>
+                Hireable: {' '}
+                {hireable ? <i className="fas fa-check text-success"></i> :
+                            <i className="fas fa-times-circle text-danger"></i>   }
+                <div className="card grid-2">
+                    <div className="all-center">
+                        <img 
+                            src={avatar_url} 
+                            alt="rount_image" 
+                            className="round-img"
+                            style={{ width: '150px'}}
+                        />
+                        <h1>{ name }</h1>
+                        <p>Location: { location }</p>
+                    </div>
+                    <div>
+                        { bio && (
+                            <Fragment>
+                                <h3>Bio:</h3>
+                                <p>{ bio }</p>
+                            </Fragment>
+                        )}
+                        <a href={html_url}
+                          className="btn btn-dark my-1">
+                          Visit Github Profile
+                        </a>
+                        <ul>
+                            <li>
+                                {login && (
+                                    <Fragment>
+                                        <strong>Username: </strong>{login}
+                                    </Fragment>
+                                )}
+                            </li>
+                            <li>
+                                {company && (
+                                    <Fragment>
+                                        <strong>Company: </strong>{company}
+                                    </Fragment>
+                                )}
+                            </li>
+                            <li>
+                                {blog && (
+                                    <Fragment>
+                                        <strong>Blog: </strong>{blog}
+                                    </Fragment>
+                                )}
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="card text-center">
+                    <div className="badge badge-primary">Followers: {followers}</div>
+                    <div className="badge badge-success">Following: {following}</div>
+                    <div className="badge badge-light">Public Repos: {public_repos}</div>
+                    <div className="badge badge-dark">Public Gists: {public_gists}</div>
+                </div>
             </Fragment>
         );
     }
