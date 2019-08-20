52b2e977ab688285016ca2fb67e2ccb919eccda7
ghigbie <gehigbie@gmail.com>
2019-08-19 21:17:30 -0600

    Add new props

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   }
@@ -39,6 +40,12 @@ class App extends Component {
   getUser = async (username) => {
     const res = await axios.get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     this.setState({ user: res.data, loading: false});
+  }
+
+  getUserRepos = async (username) => {
+    this.setState({ loading: true});
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5?sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    this.setState({ repos: res.data, loading: false});
   }
 
   clearUsers = () => this.setState({ users: [], loading: false});
@@ -73,7 +80,9 @@ class App extends Component {
                 <User 
                   { ...props }
                   getUser={this.getUser}
+                  getUserRepos={this.getUserRepos}
                   user={user}
+                  repos={repos}
                   loading={loading}
                 />
               )}/>
--- src/Components/users/User.jsx
+++ src/Components/users/User.jsx
@@ -1,7 +1,9 @@
 import React, { Component, Fragment } from 'react'
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+
 import Spinner from  './../layout/Spinner';
+import Repos from '../repos/Repos';
 
 class User extends Component {
 
@@ -9,6 +11,8 @@ class User extends Component {
         loading: PropTypes.bool.isRequired,
         user: PropTypes.object.isRequired,
         getUser: PropTypes.func.isRequired,
+        getUserRepos: PropTypes.func.isRequired,
+        repos: PropTypes.array.isRequired,
     }
 
     componentDidMount(){
@@ -29,10 +33,10 @@ class User extends Component {
             followers,
             following,
             public_repos,
-            public_gists
+            public_gists,
          } = this.props.user;
 
-         const { loading } = this.props;
+         const { loading, repos } = this.props;
 
         if (loading) return (<Spinner />);
 
@@ -99,7 +103,9 @@ class User extends Component {
                     <div className="badge badge-light">Public Repos: {public_repos}</div>
                     <div className="badge badge-dark">Public Gists: {public_gists}</div>
                 </div>
+            <Repos repos={repos}/>    
             </Fragment>
+
         );
     }
 }
